82a30d75c8b6e666f5e2035060d8f3ab517c58ed
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2021-11-11 16:37:07 +0100

    fix test.

--- src/native-test/kotlin/de/rockyj/NativeReactiveGreetingsControllerIT.kt
+++ src/native-test/kotlin/de/rockyj/NativeReactiveGreetingsControllerIT.kt
@@ -3,4 +3,4 @@ package de.rockyj
 import io.quarkus.test.junit.NativeImageTest
 
 @NativeImageTest
-class NativeReactiveGreetingResourceIT : ReactiveGreetingResourceTest()
+class NativeReactiveGreetingsControllerIT : ReactiveGreetingsControllerTest()
--- src/test/kotlin/de/rockyj/ReactiveGreetingsControllerTest.kt
+++ src/test/kotlin/de/rockyj/ReactiveGreetingsControllerTest.kt
@@ -6,7 +6,7 @@ import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
-class ReactiveGreetingResourceTest {
+class ReactiveGreetingsControllerTest {
 
     @Test
     fun testHelloEndpoint() {

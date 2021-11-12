92d50e86387b474ad01e2fc1b109c815723e09f1
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2021-11-12 18:30:08 +0100

    minor fix.

--- run_migrations.sh
+++ run_migrations.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-read_var() {
-    cat $2 | grep $1= | cut -d '=' -f2
+read_var_from_env() {
+    cat .env | grep $1= | cut -d '=' -f2
 }
 
 PWD=`pwd`
 SQL_DIR="$PWD/migrations/sql"
 
-DB_USER=$(read_var DB_USER .env)
-DB_PASSWORD=$(read_var DB_PASSWORD .env)
-DB_URL="jdbc:$(read_var DB_URL .env)"
+DB_USER=$(read_var_from_env DB_USER)
+DB_PASSWORD=$(read_var_from_env DB_PASSWORD)
+DB_URL="jdbc:$(read_var_from_env DB_URL)"
 
 docker run --rm --network=host -v ${SQL_DIR}:/flyway/sql flyway/flyway:8-alpine -user=${DB_USER} -password=${DB_PASSWORD} -url=${DB_URL} $1
